da337f50bd8d748a67b37750a426189890d77b8e
lplnt <phd@live.ru>
2022-09-28 23:59:03 +0300

    home work lessen 4

---
+++ h_w_z27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear(); //очистка консоли
+Console.Write("Введите число которое сумму которого будете считать: "); //вывод сообщение 
+int num = Convert.ToInt32(Console.ReadLine()); //ввод числа
+int suma = 0; 
+while (num > 0)
+{
+    int digit = num % 10;
+    suma = suma + digit;
+    num = num / 10;
+}
+
+Console.WriteLine(suma);
---
+++ h_w_z29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear(); //очистка консоли
+
+int[] array = new int[8];
+Random rnd = new Random();
+for(int i = 0; i < array.Length; i++ )
+{
+    array[i] = rnd.Next(0,10);
+    Console.Write(array[i]+" ");
+
+}
